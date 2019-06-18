5327598b656969f663232b76f8a77e6957d00220
André Ruffert <andre@andreruffert.com>
2019-06-18 04:18:00 +0200

    feat:  introduce JS? logo

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,7 +4,7 @@ import Logo from './Logo';
 const Intro = () => (
 	<div className="Intro">
     <Logo size={150} />
-    <h1>Questions</h1>
+    <h1>JavaScript Questions</h1>
     <p>From basic to advanced: test how well you know JavaScript, refresh your knowledge a bit, or prepare for your coding interview.</p>
     <a href="/#1" className="Button width-full">Start</a>
 	</div>
